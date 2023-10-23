cc8b27835566e89649ac80a8676da7b128ea4e98
Diogo Bernardes <Diogobernardes@ipvc.pt>
2023-10-23 22:37:31 +0100

    Added support for grayscale, resize, and rotate operations to the image processing service.

--- client.py
+++ client.py
@@ -1,13 +1,65 @@
 import xmlrpc.client
 import base64
+import tkinter as tk
+from tkinter import filedialog
 
+def select_image():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        return file_path
+    return None
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+def main():
+    proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
 
-with open("w.jpg", "rb") as f:
-    encoded_image_data = base64.b64encode(f.read()).decode('utf-8')
+    while True:
+        print("Escolha uma operação:")
+        print("1. Grayscale")
+        print("2. Redimensionar")
+        print("3. Rotacionar")
+        print("0. Sair")
+        choice = input("Opção: ")
 
-processed_image_data = proxy.convert_to_grayscale(encoded_image_data)
+        if choice == '0':
+            break
 
-with open("processed_image.jpg", "wb") as f:
-    f.write(base64.b64decode(processed_image_data))
+        if choice in ['1', '2', '3']:
+            image_path = select_image()
+            if image_path:
+                with open(image_path, "rb") as f:
+                    encoded_image_data = base64.b64encode(f.read()).decode('utf-8')
+
+                if choice == '1':
+                    task_id = proxy.create_image_processing_task(encoded_image_data, 'grayscale')
+                elif choice == '2':
+                    width = int(input("Nova largura: "))
+                    height = int(input("Nova altura: "))
+                    task_id = proxy.create_image_processing_task(encoded_image_data, 'resize', width, height)
+                elif choice == '3':
+                    angle = float(input("Ângulo de rotação (em graus): "))
+                    task_id = proxy.create_image_processing_task(encoded_image_data, 'rotate', angle)
+
+                print("Tarefa criada com ID:", task_id)
+
+                while True:
+                    task_status = proxy.get_task_status(task_id)
+                    status = task_status.get('status', 'unknown')
+                    if status == 'completed':
+                        result = task_status.get('result', '')
+                        if result:
+                            with open("processed_image.jpg", "wb") as f:
+                                f.write(base64.b64decode(result))
+                            print("Tarefa concluída. Imagem processada salva como 'processed_image.jpg'.")
+                        break
+                    elif status == 'error':
+                        print("Erro:", task_status.get('result', 'Erro desconhecido'))
+                        break
+            else:
+                print("O arquivo de imagem não foi encontrado.")
+        else:
+            print("Opção inválida. Tente novamente.")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()  # Esconder a janela principal do Tkinter
+    main()
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import xmlrpc.server
-from PIL import Image, ImageFilter
+from PIL import Image
 import base64
 from io import BytesIO
 import threading
@@ -24,9 +24,9 @@ def resize_image(encoded_image, width, height):
 def rotate_image(encoded_image, angle):
     decoded_image = base64.b64decode(encoded_image)
     image = Image.open(BytesIO(decoded_image))
-    image = image.rotate(angle)
+    rotated_image = image.rotate(angle)
     buffered = BytesIO()
-    image.save(buffered, format="JPEG")
+    rotated_image.save(buffered, format="JPEG")
     return base64.b64encode(buffered.getvalue()).decode('utf-8')
 
 def create_image_processing_task(encoded_image, operation, *args):
@@ -36,14 +36,23 @@ def create_image_processing_task(encoded_image, operation, *args):
     return task_id
 
 def process_image(task_id, encoded_image, operation, *args):
-    if operation == 'grayscale':
-        result = convert_to_grayscale(encoded_image)
-    elif operation == 'resize':
-        result = resize_image(encoded_image, *args)
-    elif operation == 'rotate':
-        result = rotate_image(encoded_image, *args)
-    TASKS[task_id]['status'] = 'completed'
-    TASKS[task_id]['result'] = result
+    try:
+        if operation == 'grayscale':
+            result = convert_to_grayscale(encoded_image)
+        elif operation == 'resize':
+            result = resize_image(encoded_image, *args)
+        elif operation == 'rotate':
+            result = rotate_image(encoded_image, *args)
+        else:
+            TASKS[task_id]['status'] = 'error'
+            TASKS[task_id]['result'] = 'Operação não suportada'
+            return
+
+        TASKS[task_id]['status'] = 'completed'
+        TASKS[task_id]['result'] = result
+    except Exception as e:
+        TASKS[task_id]['status'] = 'error'
+        TASKS[task_id]['result'] = str(e)
 
 def get_task_status(task_id):
     task = TASKS.get(task_id, {})
@@ -51,9 +60,6 @@ def get_task_status(task_id):
 
 server = xmlrpc.server.SimpleXMLRPCServer(("0.0.0.0", 8000))
 print("Listening on port 8000...")
-server.register_function(convert_to_grayscale, "convert_to_grayscale")
-server.register_function(resize_image, "resize_image")
-server.register_function(rotate_image, "rotate_image")
 server.register_function(create_image_processing_task, "create_image_processing_task")
 server.register_function(get_task_status, "get_task_status")
 server.serve_forever()
