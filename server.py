8819b78262dd22731b0f03886cc6e51cd9cd699d
Diogo Bernardes <Diogobernardes@ipvc.pt>
2023-10-27 09:38:25 +0100

    Added possibility to edit png images

--- server.py
+++ server.py
@@ -5,15 +5,16 @@ from io import BytesIO
 
 def convert_to_grayscale(encoded_image):
     decoded_image = base64.b64decode(encoded_image)
-    image = Image.open(BytesIO(decoded_image)).convert('L')
+    image = Image.open(BytesIO(decoded_image))
+    image = image.convert('L')  # Convert to grayscale
     buffered = BytesIO()
-    image.save(buffered, format="JPEG")
+    image.save(buffered, format="JPEG")  # Save as JPEG
     return base64.b64encode(buffered.getvalue()).decode('utf-8')
 
 def resize_image(encoded_image, width, height):
     decoded_image = base64.b64decode(encoded_image)
     image = Image.open(BytesIO(decoded_image))
-    image = image.resize((width, height),Image.LANCZOS)
+    image = image.resize((width, height), Image.LANCZOS)
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
     return base64.b64encode(buffered.getvalue()).decode('utf-8')
@@ -32,4 +33,3 @@ server.register_function(convert_to_grayscale, "convert_to_grayscale")
 server.register_function(resize_image, "resize_image")
 server.register_function(rotate_image, "rotate_image")
 server.serve_forever()
-         
